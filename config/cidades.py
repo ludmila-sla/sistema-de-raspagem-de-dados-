808e8c61895097c8c7f4b70282396e37db441d24
Ludmila Fernandes <54753099+ludmila-sla@users.noreply.github.com>
2026-06-12 11:17:10 -0300

    Add APS dictionary with city names

--- config/cidades.py
+++ config/cidades.py
@@ -1,1 +1,24 @@
-
+APS = {
+  "bauru": [
+    "Bauru",
+    "Piratininga
+  ],
+  "são josé do rio preto": [
+    "São José do Rio Preto",
+    "Bady Bassit",
+    "Cedral",
+    "Guapiaçu",
+    "Mirassol
+  ],
+  "são josé dos campos": [
+    "São José dos Campos",
+    "Caçapava",
+    "Jacarei"
+  ],
+  "sorocaba": [
+    "Sorocaba",
+    "Araçoiaba da Serra",
+    "Aluminio",
+    "Votarantim"
+  ]
+}
